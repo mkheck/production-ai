08f3ffb5cbef86407d96ae37ee9e17950b1855fd
Mark Heckler <mark.heckler@gmail.com>
2024-01-22 11:13:11 -0600

    Initial commit

---
+++ src/main/java/com/thehecklers/productionai/ProductionAiApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.productionai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thehecklers/productionai/ProductionAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.productionai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
