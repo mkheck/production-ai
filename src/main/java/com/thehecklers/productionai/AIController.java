413c99fb2d9a18a805bd85c1122f001921f8d103
Mark Heckler <mark.heckler@gmail.com>
2024-01-22 13:30:16 -0600

    Added template-based gen endpoint

--- src/main/java/com/thehecklers/productionai/AIController.java
+++ src/main/java/com/thehecklers/productionai/AIController.java
@@ -1,11 +1,14 @@
 package com.thehecklers.productionai;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 import org.springframework.ai.azure.openai.AzureOpenAiChatClient;
+import org.springframework.ai.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/")
@@ -18,11 +21,18 @@ public class AIController {
 
     // @GetMapping
     // public String livenessCheck() {
-    //     return "Hello, AI!";
+    // return "Hello, AI!";
     // }
 
     @GetMapping
     public String generateResponse(@RequestParam(defaultValue = "What is the meaning of life") String prompt) {
         return client.generate(prompt);
+    }
+
+    @GetMapping("/template")
+    public String generateResponseFromTemplate(@RequestParam String requestType, @RequestParam String requestTopic) {
+        var template = new PromptTemplate("Tell me a {type} about {topic}",
+                Map.of("type", requestType, "topic", requestTopic));
+        return client.generate(template.create()).getGeneration().getContent();
     }
 }
