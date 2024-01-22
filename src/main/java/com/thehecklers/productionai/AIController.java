d396095e74ebb3bcee7da2b61dc3f02d0018c74c
Mark Heckler <mark.heckler@gmail.com>
2024-01-22 11:37:57 -0600

    String/String prompt and response

--- src/main/java/com/thehecklers/productionai/AIController.java
+++ src/main/java/com/thehecklers/productionai/AIController.java
@@ -1,15 +1,28 @@
 package com.thehecklers.productionai;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.azure.openai.AzureOpenAiChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 @RequestMapping("/")
 public class AIController {
+    private final AzureOpenAiChatClient client;
+
+    public AIController(AzureOpenAiChatClient client) {
+        this.client = client;
+    }
+
+    // @GetMapping
+    // public String livenessCheck() {
+    //     return "Hello, AI!";
+    // }
+
     @GetMapping
-    public String livenessCheck() {
-        return "Hello, AI!";
+    public String generateResponse(@RequestParam(defaultValue = "What is the meaning of life") String prompt) {
+        return client.generate(prompt);
     }
 }
